eea03ccd0432bfe745ac1759f96e9f96cbbba73d
Caleb <daikuweb@gmail.com>
2021-07-08 20:38:46 -0500

    Post Component UI

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 
 import Home from './screens/home'
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Home/>
-      <StatusBar style="light" />
-    </View>
+      <StatusBar style="auto" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  container:{
+    flex: 1
+  }
 });
--- components/post/style.js
+++ components/post/style.js
@@ -13,6 +13,69 @@ const styles = StyleSheet.create({
         bottom: 0,
         right: 0,
     },
+    uiContainer:{
+        height: '100%',
+        justifyContent: 'flex-end',
+    },    
+    bottomContainer:{
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end'
+    },
+    handle:{
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '700',
+        marginBottom: 7,
+    },
+    description:{
+        color: '#fff',
+        fontSize: 15,
+        fontWeight: '100',
+        marginBottom: 10,
+    },
+    songRow:{
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    songName:{
+        color: '#fff',
+        fontSize: 16,
+        marginLeft: 5,
+        
+    },
+    songImg:{
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        borderWidth: 7,
+        borderColor: '#252525',
+    },
+
+    rightContainer:{
+        alignSelf: 'flex-end',
+        height: 300,
+        justifyContent: 'space-between',
+        marginRight: 5,
+    },
+    profilePicture:{
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        borderWidth: 2,
+        borderColor: '#fff',
+    },
+    iconContainer:{
+        alignItems: 'center',
+    },
+    label:{
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '600',
+        marginTop: 5,
+    },
+    
 });
 
 export default styles;
