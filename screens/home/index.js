f3a565cca50d32aa5e71a0583241a2354226b831
Caleb <daikuweb@gmail.com>
2021-07-08 21:04:43 -0500

    Post Component Props & Fixed Pause Bug

--- screens/home/index.js
+++ screens/home/index.js
@@ -3,10 +3,26 @@ import { Text, View } from 'react-native'
 
 import Post from '../../components/post'
 
+const post1 = {
+    id: 'p1',
+    videoUri: 'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+    user: {
+      id: 'u1',
+      username: 'daviddobrik',
+      imageUri: 'https://yt3.ggpht.com/ytc/AAUvwni9qPKK52ivOihzIBPtdLG4IHzXyUfHUMEG-fHVDQ=s900-c-k-c0x00ffffff-no-rj',
+    },
+    description: 'hahahah oh boy @borat',
+    songName: 'Indiomar - Sentidos',
+    songImage: 'https://i1.sndcdn.com/artworks-9Ijoi9T4CkOC-0-t500x500.jpg',
+    likes: 123,
+    comments: 12,
+    shares: 44,
+}
+
 const Home = () => {
     return (
         <View>
-            <Post/>
+            <Post post={post1}/>
         </View>
     )
 }
